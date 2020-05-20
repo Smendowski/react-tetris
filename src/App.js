74818c6f44cf56b93e1e8763437807269b47870b
Smendowski <m.smendowski@gmail.com>
2020-05-20 20:28:11 +0200

    Multi Page App render Prototype

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Tetris from './components/Tetris';
+import ContinueButton from './components/ContinueButton';
 
-const App = () => (
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { isEmptyState: true }
+  }
 
+  ContinueVisiting = () => {
+    this.setState({
+      ...this.state,
+      isEmptyState: false,
+      isContinueState: true
+    })
+  }
 
-  <div className="App">
-    <Tetris />
-  </div>
-);
+  render() {
+    return (
+      <div>
+        {this.state.isEmptyState && <ContinueButton continue={this.ContinueVisiting} />}
+  
+        {this.state.isContinueState  && <Tetris />}
+      </div>
+    )
+  }
+  
+}
 
 export default App;
---
+++ src/components/ContinueButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ContinueButton = props => {
+    return <button onClick={props.continue}>Continue</button>
+}
+
+export default ContinueButton;
