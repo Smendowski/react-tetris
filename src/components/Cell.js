cac255d97b6cc7f25e2894c7271220fe38f95bac
Smendowski <m.smendowski@gmail.com>
2020-05-20 09:54:32 +0200

    Revert "Prototype Game Info displaying"
    This reverts commit 4e8d44457796268e5452e9d59b612140fa57e20d.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -7,5 +7,4 @@ const Cell = ({ type }) => (
     <StyledCell type={type} color={BLOCKS[type].color}/>
 )
 
-export default React.memo(Cell);
-// React memo - optimization, we rerendder if cell has changed :)
+export default Cell;
